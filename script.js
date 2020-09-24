1636c6d3adbaf19f14a16548a96188811f39e5c6
ethickfox <ethickfox@gmail.com>
2020-09-24 10:55:05 +0300

    Finished second task

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
-$( "#advicer" )
-.keypress(function () {
-    // filterFunction()
+$("#advicer")
+.keyup(async function () {
+    var input = document.getElementById('advicer').value;
+    var result ="";
+    if(input!=""){
+        result = await search(input)
 
-    // search($(this).val)
-// search2()
-// search().then(res=>console.log(res))
-search()
-
-    // $('#dialog_title_span').text("new dialog title");
-
-
+        if(result!=""){
+            displayItems(result)
+        }else{
+            clearItems()
+        }
+    }else{
+        clearItems()
+    }
 })
 .change();
 
-// function filterFunction() {
-//     var input, filter, ul, li, a, i;
-//     input = document.getElementById("advicer");
-//     filter = input.value.toUpperCase();
-//     div = document.getElementById("myDropdown");
-//     a = div.getElementsByTagName("a");
-//     for (i = 0; i < a.length; i++) {
-//       txtValue = a[i].textContent || a[i].innerText;
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         a[i].style.display = "";
-//       } else {
-//         a[i].style.display = "none";
-//       }
-//     }
-//   }
-async function search(){
+function displayItems(items){
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    i=0;
+    for(i=0;i<items.length;i++){
+        a[i].style.display = "block";
+        a[i].textContent=items[i].value
+    }
+}
+function clearItems(){
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for(i=0;i<10;i++)
+        a[i].style.display = "none";
+
+}
+$("#myDropdown").click(
+    async function( event ) {
+        var target = $( event.target );
+        if ( target.is( "a" ) ) {
+            document.getElementById('advicer').value = target[0].outerText
+            result = await search(target[0].outerText)
+            displayItems(result)
+        }
+    }
+)
+async function search(query){
     var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
     var token = "8b774b12d8ec56660580095dc0fa64a27730a560";
-    var query = "москва хабар";
-    
     var options = {
         method: "POST",
         mode: "cors",
@@ -44,8 +56,13 @@ async function search(){
         body: JSON.stringify({query: query})
     }
     
-    fetch(url, options)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log("error", error));
+    let resp = await fetch(url, options)
+    
+    if (resp.ok) {
+        let json = await resp.json();
+        return json.suggestions
+    } else {
+        alert("Ошибка HTTP: " + response.status);
+        return ""
+    }
 }
